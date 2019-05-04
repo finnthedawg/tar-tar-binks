7d6fe64846353f0b8bb38c571afea8a96c4c5bf7
SamSamhuns <samhunsadamant@gmail.com>
2019-05-04 17:29:10 +0400

    Add cmd arg validation check in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,47 @@
-#include <iostream>
+/*
+ * Function and variable naming convention used
+ * all variables = camelCase
+ * all function names = function_do_undscr()
+ * structs
+ */
+#include <set>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <iterator>
 #include "common.h"
 
+bool chk_cmd_args(int argc, char *argv[]);
+
 int main(int argc, char *argv[]) {
+	/* Checking for valid cmd args */
+	if (chk_cmd_args(argc, argv) == false ) {
+		// incorrect args error exit
+		std::cout << "Incorrect Usage: tartar -flag <archive-file> <file/directory list>" << '\n';
+		return 0;
+	}
+	std::cout << "TARTAR WILL NOW COMMENCE\n";
 
-    /* Checking for correct number of arguments */
-    if ( argc != 4 ) {
-        std::std::cout << "Incorrect Usage: adtar -flag <archive-file> <file/directory list>" << '\n';
-    }
+	return 0;
+}
 
-    std::cout << "ADTAR WILL NOW COMMENCE\n";
+bool chk_cmd_args(int argc, char *argv[]) {
+	if (argc < 4) {
+		return false;
+	}
 
-    std::string inputS;
-    std::getline(std::cin, inputS);
-    std::cout << "It is " << inputS << std::endl;
-    return 0;
+	std::set<std::string> vFlag = {"-c", "-a", "-x", "-m", "-p"}; // set of valid flags
+	const bool isValidFlag = vFlag.find(argv[1]) != vFlag.end();  // checks if the entered flag is valid
+
+	if (isValidFlag == false) {                                   // checking for invalid flags
+		return false;
+	}
+    // checking if -x flag used correctly
+	if ((std::string)argv[1] == "-x" && (std::string)argv[2] == "-o" && argc == 6) {
+		return true;
+	}
+	if ( argc != 4 ) {
+		return false;
+	}
+	return true;
 }
