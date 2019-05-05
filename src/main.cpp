2f4fd78d71a51e16463dfde624dbefd83299f06f
SamSamhuns <samhunsadamant@gmail.com>
2019-05-05 23:25:53 +0400

    Store argvs in archiveFile and inputDir. Add debug statements

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,28 @@
 #include <iostream>
 #include "common.h"
 
-bool chk_cmd_args(int argc, char *argv[]);
+bool chk_cmd_args(int argc, char *argv[], std::string& archiveFile, std::string& inputDir);
 
 int main(int argc, char *argv[]) {
+	std::string archiveFile, inputDir;
 	/* Checking for valid cmd args */
-	if (chk_cmd_args(argc, argv) == false ) {
+	if (chk_cmd_args(argc, argv, archiveFile, inputDir) == false ) {
 		// incorrect args error exit
 		std::cerr << "Incorrect Usage: tartar -flag <archive-file> <file/directory list>" << '\n';
 		return 0;
 	}
-	std::cout << "DEBUG TARTAR WILL NOW COMMENCE\n";
 
-	
+	std::cout << "DEBUG TARTAR WILL NOW COMMENCE\n" \
+		<< archiveFile << inputDir << "should have smth before" << std::endl;
+
+
 
 
 	return 0;
 }
 
 /* Function to check for illegal args while invoking the tar archiver */
-bool chk_cmd_args(int argc, char *argv[]) {
+bool chk_cmd_args(int argc, char *argv[], std::string& archiveFile, std::string& inputDir) {
 	if (argc < 4) {
 		return false;
 	}
@@ -41,10 +44,14 @@ bool chk_cmd_args(int argc, char *argv[]) {
 	}
     // checking if -x flag used correctly
 	if ((std::string)argv[1] == "-x" && (std::string)argv[2] == "-o" && argc == 6) {
+		archiveFile = (std::string)argv[4];
+		inputDir = (std::string)argv[5];
 		return true;
 	}
 	if ( argc != 4 ) {
 		return false;
 	}
+	archiveFile = (std::string)argv[2];
+	inputDir = (std::string)argv[3];
 	return true;
 }
