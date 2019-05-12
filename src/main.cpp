5f74036802c37c02fcb66b6fff864a2c97d89be4
SamSamhuns <samhunsadamant@gmail.com>
2019-05-12 11:49:14 +0400

    Add test run for the -c flag

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <csignal>
 #include <iostream>
+#include <dirent.h>
 #include "meta.h"
 #include "store.h"
 #include "common.h"
@@ -24,13 +25,8 @@
 bool chk_cmd_args(int argc, char *argv[], std::string& archiveName,
                   std::vector<std::string>& inputList, std::string& flag, int& version);
 
-void signalHandler( int signum ) {
-	std::cerr << "Interrupt signal (" << signum << ") received.\n";
-	std::cerr << "Incorrect Usage: tartar -flag <archive-file> <file/directory list>" << std::endl;
-	// cleanup and close up stuff here
-	// terminate program
-	exit(signum);
-}
+/* Signal Handler for catching SIGABRT signals */
+void signalHandler( int signum );
 
 int main(int argc, char *argv[]) {
 	// register signal SIGSEGV and signal handler
@@ -38,11 +34,12 @@ int main(int argc, char *argv[]) {
 	std::string archiveName, flag;
 	std::vector<std::string> inputList;
 	int version; // will be equal to -1 if -o version number is not specified
+    Header mainHeader;
 
 	/* Checking for valid cmd args */
 	if (chk_cmd_args(argc, argv, archiveName, inputList, flag, version) == false ) {
 		// incorrect args error exit
-		std::cerr << "Incorrect Usage: tartar -flag <archive-file> <file/directory list>" << std::endl;
+		std::cerr << "Incorrect Usage: ./tartar -flag <archive-file> <file/directory list>" << std::endl;
 		return 1;
 	}
 
@@ -57,43 +54,66 @@ int main(int argc, char *argv[]) {
 	std::cout << "DEBUG TARTAR WILL NOW COMMENCE\n" \
 	          << "Archive Name is " <<archiveName << ". Version is "<< version << std::endl;
 
-	// Loop through the inputList for all flags except for the -m and -p flag
+	// Check the flag type and run the appropriate command
 	switch (flag[1]) {
-	case 'c':
+	case 'c': // -c stpre flag
 		std::cout << "DEBUG -c flag used. Files/dirs are:" << '\n';
-		// iterating through the inputList cector to print everything out
+		// iterating through the inputList vector to get all the files/dirs to archive
 		for(std::vector<int>::size_type i = 0; i != inputList.size(); i++) {
 			std::cout << "DEBUG " << inputList[i] << std::endl;
+            iterate_through_dir(inputList[i], archivePtr, mainHeader);
 		}
 		break;
-	case 'a':
+	case 'a': // -a append flag
 		std::cout << "DEBUG -a flag used. Files/dirs are:" << '\n';
-		// iterating through the inputList cector to print everything out
+		// iterating through the inputList vector to append all the stated files
 		for(std::vector<int>::size_type i = 0; i != inputList.size(); i++) {
 			std::cout << "DEBUG " << inputList[i] << std::endl;
+
+			// TODO run the append function for each file/dir in the inputList
 		}
 		break;
-	case 'x':
+	case 'x': // -x extract flag with/without -o version flag
 		std::cout << "DEBUG -x flag used. Files/dirs are:" << '\n';
-		// iterating through the inputList cector to print everything out
+		// iterating through the inputList vector to extract all the stated files/ non-empty dirs
 		for(std::vector<int>::size_type i = 0; i != inputList.size(); i++) {
 			std::cout << "DEBUG " << inputList[i] << std::endl;
+
+			// TODO run the extract function for each file/dir in the inputList
 		}
 		break;
-	case 'm':
+	case 'm': // -m print metatdata flag
+		// TODO print out meta data
 		std::cout << "DEBUG -m flag." << '\n';
+
+        /* If no files are specified then print out Metadata of all files in archive */
+        if (argc == 3) {
+            /* code */
+        }
+        // iterating through the inputList vector to print the metadata for all mentioned files/folders
+        for(std::vector<int>::size_type i = 0; i != inputList.size(); i++) {
+            std::cout << "DEBUG " << inputList[i] << std::endl;
+
+            // TODO run the extract function for each file/dir in the inputList
+        }
 		break;
-	case 'p':
+	case 'p': // -p print directory hierarchy flag
+		// TODO print the directory hierarchy
 		std::cout << "DEBUG -p flag." << '\n';
 		break;
 	default:
 		std::cout << "ERROR: invalid flags" << '\n';
 	}
-
-
-
 	archivePtr.close();
 	return 0;
+}
+
+void signalHandler( int signum ) {
+	std::cerr << "Interrupt signal (" << signum << ") received.\n";
+	std::cerr << "Correct Usage: ./tartar -flag <archive-file> <file/directory list>" << std::endl;
+	// cleanup and close up stuff here
+	// terminate program
+	exit(signum);
 }
 
 /* Function to check for illegal args while invoking the tar archiver */
