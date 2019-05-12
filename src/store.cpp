101ea7ae27697623b916f6a835a8e6b43939176c
SamSamhuns <samhunsadamant@gmail.com>
2019-05-12 16:02:05 +0400

    Add function for creating an archive file

--- src/store.cpp
+++ src/store.cpp
@@ -1,3 +1,30 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <dirent.h>
+#include <sys/stat.h>
 #include "store.h"
+#include "common.h"
 
 /* Implementation for -c flag*/
+/* function to create an archive file */
+int store_archive(std::vector<std::string> inputList, std::fstream &archivePtr,
+                  struct Header &mainHeader, std::vector<struct Metadata>& metaVector) {
+
+
+	// iterating through the inputList vector to get all the files/dirs to archive
+    // iterate_through_dir write the contents of all files that have been found
+    // starting from the mainHeader.offsetToMeta
+	for(std::vector<int>::size_type i = 0; i != inputList.size(); i++) {
+		std::cout << "DEBUG " << inputList[i] << std::endl;
+		iterate_through_dir(inputList[i], archivePtr, mainHeader);
+	}
+	write_header_to_disk(mainHeader, archivePtr);
+
+	//TODO
+	// write_metadata_to_disk(long long offset, std::vector <struct Metadata> metaVector);
+
+	return -1;
+}
