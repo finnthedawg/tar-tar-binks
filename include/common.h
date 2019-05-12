aaaf220c87e637ee385c95109ecd4dedfd471b23
SamSamhuns <samhunsadamant@gmail.com>
2019-05-12 16:02:57 +0400

    Add header write, append file to disk and update metadata func defs

--- include/common.h
+++ include/common.h
@@ -36,6 +36,18 @@ struct Metadata {
 
 /* Recursive iteration through directories in disk and add the information about the file/directory to
     the metadata vector and Immediately write to archive file on disk if its a file */
-void iterate_through_dir(std::string dirName, std::fstream& archivePtr, struct Header);
+void iterate_through_dir(std::string baseDirName, std::fstream &archivePtr, struct Header &mainHeader);
+
+/* Write header to disk */
+int write_header_to_disk(struct Header &mainHeader, std::fstream &archivePtr);
+
+/* Add updated version or new Meta struct to Metadata in memory when using -a APPEND flag */
+/* When using -a append flag, search through Global Metadata Struct to see if a prev ver exists
+   Push an updated version meta to vector if found
+   Push a new meta to vector if not found */
+int update_metadata_in_memory(std::vector <struct Metadata> &metaVector, std::fstream &readFilePtr);
+
+/* Append/Write the file to the archive on disk for -c CREATE and -a APPEND flag */
+int append_file_to_disk(std::fstream &archivePtr, std::string pathToObject, struct Header &mainHeader);
 
 #endif
