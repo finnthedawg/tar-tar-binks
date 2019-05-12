6c976eda1c5579041147bbed85ecd6d85d42d81a
SamSamhuns <samhunsadamant@gmail.com>
2019-05-12 16:03:13 +0400

    Add decalration for store_archive func

--- include/store.h
+++ include/store.h
@@ -1,5 +1,8 @@
 #ifndef STORE_H // Making sure the header files are not included twice
 #define STORE_H
 
+/* function to create an archive file */
+int store_archive(std::vector<std::string> inputList, std::fstream &archivePtr,
+                  struct Header &mainHeader, std::vector<struct Metadata>& metaVector);
 
 #endif
