9f064965ae8671f949fbedc82460b1073b3b66ba
SamSamhuns <samhunsadamant@gmail.com>
2019-05-13 16:14:23 +0400

    Add def for displaying hierarchy of all objects

--- include/hierarchy.h
+++ include/hierarchy.h
@@ -1,5 +1,7 @@
 #ifndef HIERARCHY_H // Making sure the header files are not included twice
 #define HIERARCHY_H
 
+/* display heirarchy of all objects in metaVector */
+void display_hierarchy_from_archive (std::vector <struct Metadata> metaVector);
 
 #endif
