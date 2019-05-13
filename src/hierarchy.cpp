09717734d489d94942d3a38b611d104660cca608
SamSamhuns <samhunsadamant@gmail.com>
2019-05-13 16:14:45 +0400

    Add display hierarchy function

--- src/hierarchy.cpp
+++ src/hierarchy.cpp
@@ -1,3 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sys/stat.h>
+#include "common.h"
 #include "hierarchy.h"
 
 /* Implementation for -p flag*/
+/* display heirarchy of all objects in metaVector */
+void display_hierarchy_from_archive (std::vector <struct Metadata> metaVector) {
+    for(std::vector<int>::size_type i = 0; i != metaVector.size(); i++) {
+        std::cout << metaVector[i].pathToObject << '\n';
+    }
+}
