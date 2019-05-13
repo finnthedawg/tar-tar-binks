fa1f25f9518ebd832073a49b2134be8fd6ff9630
SamSamhuns <samhunsadamant@gmail.com>
2019-05-13 23:12:08 +0400

    Append.ccp makes a call to add_objects_to_archive

--- include/append.h
+++ include/append.h
@@ -1,5 +1,8 @@
 #ifndef APPEND_H // Making sure the header files are not included twice
 #define APPEND_H
 
+/* function to append to the archive file */
+int append_archive (std::vector<std::string> inputList, std::fstream &archivePtr,
+                    struct Header &mainHeader, std::vector<struct Metadata>& metaVector, char flag);
 
 #endif
--- src/append.cpp
+++ src/append.cpp
@@ -1,3 +1,18 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <dirent.h>
+#include <sys/stat.h>
 #include "append.h"
+#include "common.h"
 
 /* Implementation for -a flag*/
+/* function to append to the archive file */
+int append_archive (std::vector<std::string> inputList, std::fstream &archivePtr,
+                    struct Header &mainHeader, std::vector<struct Metadata>& metaVector, char flag) {
+
+	add_objects_to_archive(inputList, archivePtr, mainHeader, metaVector, 'a');
+	return 0;
+}
