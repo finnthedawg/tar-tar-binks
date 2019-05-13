8ea499077d65a893a1d2ddd67935cca5a0810044
SamSamhuns <samhunsadamant@gmail.com>
2019-05-13 14:13:18 +0400

    Add skeleton def for printing meta from metaVector in meta.cpp

--- src/meta.cpp
+++ src/meta.cpp
@@ -1,3 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sys/stat.h>
 #include "meta.h"
+#include "common.h"
 
 /* Implementation for -m flag*/
+void display_metadata_from_archive (std::vector <struct Metadata> metaVector) {
+    for(std::vector<int>::size_type i = 0; i != metaVector.size(); i++) {
+
+        std::cout << metaVector[i].fileName << '\n';
+    }
+}
