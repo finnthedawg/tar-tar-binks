4f96ec0327a257de40db1bb135a0f8b1d381d605
SamSamhuns <samhunsadamant@gmail.com>
2019-05-13 16:14:10 +0400

    Add defs for display all meta attrbs of one meta and for all metas in metaVector

--- include/meta.h
+++ include/meta.h
@@ -1,5 +1,9 @@
 #ifndef META_H // Making sure the header files are not included twice
 #define META_H
 
+/* Implementation for -m flag*/
+int display_metadata_from_archive (std::vector <struct Metadata> metaVector, std::string pathToObject, bool displayAll);
 
+/* display all attributes on Metadata object */
+void display_one_metadata_attrb (struct Metadata metadata_object);
 #endif
